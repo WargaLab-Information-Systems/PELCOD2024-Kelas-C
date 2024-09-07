64c9103c3199526fd65b8d6a235d4f38687cfcac
LUSI-PRATICYA-DINATA <praticya054@gmail.com>
2024-09-07 11:49:45 +0700

    Add files via upload

---
+++ 240441100015_LUSI_LATIHAN.py
@@ -0,0 +1,21 @@
+# nama = "Lusi"
+# umur = 18
+# nilaiAlpro = 99.9
+
+# hasil = f"Nama Saya, {nama} Umur Saya {umur} dan Nilai Alpro saya {nilaiAlpro}"
+
+# print(hasil)
+# print("Nama Saya", nama, "Umur : ", umur, "Nilai Alpro: ", nilaiAlpro)
+
+# mandi = input("Apakah kamu sudah mandi? ")
+# cuaca = input("Cuaca sekarang seperti apa? ")
+
+# if mandi == "sudah" and cuaca == "cerah" :
+#     print("Ayo berangkat ngampus")
+
+bilangan1 = int(input("Masukan Bilangan Pertama: "))
+bilangan2 = int(input("Masukan Bilangan Kedua "))
+
+hasil = bilangan1 + bilangan2
+
+print("Hasil Penjumlahan dari", bilangan1 , "ditambah" , bilangan2 , "adalah: ", hasil)
