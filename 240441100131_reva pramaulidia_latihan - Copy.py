6772e5dc85639fd0b384abe34755a1a8a9afb3ad
revaprama <pramaulidiareva@gmail.com>
2024-09-07 11:56:48 +0700

    Add files via upload

---
+++ 240441100131_reva pramaulidia_latihan - Copy.py
@@ -0,0 +1,20 @@
+# nama="reva"
+# umur=18
+# nilaialpro=99.9
+
+# hasil=f"nama saya {nama} umur saya {umur} dan nilai alpro {nilaialpro}"
+
+# print (hasil)
+
+# makan = input ("apakah kamu sudah makan ")
+# cuaca = input ("cuaca sedang cerah")
+
+# if makan == "sudah" and cuaca == "cerah":
+#     print ("ayo cari makan")
+
+bilangan1= input("masukan bilangan pertama")
+bilangan2= input("masukan bilangan kedua")
+
+hasil = bilangan1 + bilangan2
+print ("hasil penjumlahan dari",bilangan1,"ditambah", bilangan2,"adalah", hasil)
+
