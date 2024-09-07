c6a5cd2ef7aff433e58015343964be542b64178e
rizqitamartha <rizqitamartha@gmail.com>
2024-09-07 11:50:11 +0700

    Add files via upload

---
+++ 240441100027_RIZQITA MARTHA AMALIA_Latihan.py
@@ -0,0 +1,21 @@
+# nama = "Shin Tae Yong"
+# umur = 21
+# nilaiAlpro = 90.1
+
+# hasil = f"Nama saya, {nama} Umur saya {umur} dan Nilai Alpro saya {nilaiAlpro}"
+
+# print(hasil)
+# print("Nama Saya ", nama, " Umur : ", umur, "Nilai Alpro: ", nilaiAlpro)
+
+# mandi = input("Apakah kamu sudah mandi? ")
+# cuaca = input("Cuaca Sekarang seperti apa? ")
+
+# if mandi == "sudah" and cuaca == "cerah":
+#     print("Ayo berangkat ngampus")
+
+bilangan1 = int(input("Masukkan Bilangan Pertama: "))
+bilangan2 = int(input("Masukkan Bilangan Kedua: "))
+
+hasil = bilangan1 + bilangan2
+
+print("Hasil Penjualan dari", bilangan1 ,"ditambah" , bilangan2 ,"adalah: ", hasil)
