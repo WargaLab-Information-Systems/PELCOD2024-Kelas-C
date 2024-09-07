e38510ddaacd1989f608ec9a9a729ae06b59d29d
nindasihotang <nindasihotang@gmail.com>
2024-09-07 11:54:38 +0700

    Add files via upload

---
+++ 240441100003_ninda_latihan.py
@@ -0,0 +1,21 @@
+# nama ="ninda"
+# umur =17
+# nilaiAlpro =90.1
+
+# hasil = f"Nama saya,{nama} Umur saya {umur} dan Nilai Alpro saya {nilaiAlpro}"
+
+# print(hasil)
+# print (" Nama saya" , nama , "umur :" , umur, "Nilai Alpro" , nilaiAlpro)
+
+# mandi = input ("Apakah kamu sudah mandi ?")
+# cuaca = input ("Cuaca sekarang seperti apa ?")
+
+# if mandi == " sudah" and cuaca == "cerah":
+#     print("Ayo berangkat ngampus")
+
+bilangan1 = int(input("Masukkan Bilangan pertama: "))
+bilangan2 = int(input("Masukkan Bilangan kedua: "))
+
+hasil = bilangan1 + bilangan2 
+
+print("Hasil penjumlahan dari", bilangan1 ,"ditambah" , bilangan2 , "adalah: " , hasil)
