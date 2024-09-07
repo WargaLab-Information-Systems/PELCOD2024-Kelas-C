5d7cd3d7eecc89a6049632f7c082a1c4b8292890
Ediputra07 <edi075412@gmail.com>
2024-09-06 21:54:56 -0700

    240441100011_Edi Putra_

---
+++ main.py
@@ -0,0 +1,25 @@
+# # nama = "Edi Putra"
+# # umur = 17
+# # nilai= 95
+
+# # hasil = f"nama saya {nama} umur saya {umur} dan nilai alpro saya {nilai}"
+
+# # print(hasil)
+# # print("nama saya:",nama, "umur saya:",umur, "nilai alpro saya:",nilai)
+
+# # mandi = input("apakah kamu sudah mandi?")
+# # cuaca = input("cuaca sekarang seperti apa?")
+# # makan = input("udah makan yang?")
+
+# # if mandi =="sudah" and cuaca =="cerah":
+# #     print("ayo berangkat ngampus")
+# # if mandi =="malas" or cuaca =="panas":
+# #     print("ngapain ngampus")
+# # if makan =="belom" and 
+
+# bilangan1 = int(input("masukkan bilangan pertama: "))
+# bilangan2 = int(input("masukkan bilangan kedua: "))
+
+# hasil = bilangan1 - bilangan2
+
+# print("hasil penjumlahan dari", bilangan1, "dikurangi", bilangan2, "adalah", hasil)
