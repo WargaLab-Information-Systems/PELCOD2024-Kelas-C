590f5df7ce3f10c17e455550fe2f251c08d41e71
dytrgn <tariganadytta41@gmail.com>
2024-09-07 11:56:48 +0700

    Add files via upload

---
+++ 240441100139_Adytta Putra Tarigan.py
@@ -0,0 +1,22 @@
+# # nama= "Adytta Putra Tarigan"
+# # umur= 19
+# # nilaialpro= 95.0
+
+# # hasil= f"nama saya, {nama} umur saya {umur} dan nilai alpro saya {nilaialpro}"
+# # print("Nama saya", nama, "umur",umur, "nilai alpro", nilaialpro)
+
+
+
+
+# mandi = input("apakah kamu sudah mandi?")
+# cuaca = input("Cuaca sekarang seperti apa?")
+
+# if mandi == "sudah" and cuaca == "cerah":
+#     print("ayo berangkat ngampus")
+
+bilangan1 = int(input("masukkan bilangan pertama: * "))
+bilangan2 = int(input("masukkan bilangan kedua: * "))
+
+hasil = bilangan1 + bilangan2
+
+print("hasil penjumlahan dari", bilangan1 ,"ditambah" , bilangan2 ,"adalah: ", hasil)
