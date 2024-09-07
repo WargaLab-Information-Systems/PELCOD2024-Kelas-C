11edf324b2a2c8ce67683681bb53697ac4ee197c
Revan Novanda A.Y. <revannovanda2005@gmail.com>
2024-09-07 22:21:06 +0700

    Add files via upload

---
+++ 240441100147_Revan Novanda Anandita Yahya_Tugas dialog.py.py
@@ -0,0 +1,42 @@
+nama1 = "Revan"
+nama2 = "Bimo"
+alamat1 = "Kediri"
+alamat2 = "Malang"
+hobi1 = "coding"
+hobi2 = "belajar bahasa pemrograman python"
+umur1 = 20
+umur2 = 18
+
+print("Di suatu pagi yang biasa-biasa saja bertemulah dua insan maba yang tidak sengaja bertemu di taman kampus")
+
+print("Revan: Halo, nama saya", nama1)
+print("Bimo: Hi, nama saya adalah", nama2)
+
+print("Revan: Wow! nama yang keren, kamu berasal darimana?")
+print("Bimo: Aku berasal dari", alamat2)
+print("Revan: Hebat, kalau aku dari", alamat1)
+
+print("Bimo: Kamu umur berapa?")
+print("Revan: Aku umur", umur1)
+print("Bimo: Wah, kita selisih 2 tahun kalau aku umur", umur2)
+
+umur1 = int(input("masukkan umur1 :"))
+umur2 = int(input("masukkan umur2 :"))
+
+hasil = umur1 - umur2 
+print("hasil dari pengurangan dari", umur1, "dikurangi", umur2, "adalah", hasil)
+
+print("Revan: Kamu punya hobi?")
+print("Bimo: Punya dong, hobiku", hobi2)
+print("Revan: Kalau aku hobinya", hobi2)
+
+Revan = input("Apakah hobi kita sama?")
+Bimo = input("Apakah kamu mau belajar coding bersama?")
+
+if Revan == "iya" and Revan == "gas!":
+     print("Wah! hobi kita sama")
+    
+print("Revan: Kalau gitu kita belajar di Perpustakaan Gedung Cakra aja ya")
+print("Bimo: Gaaaaaaas!")
+
+print("Akhirnya dua insan maba ini belajar bahasa pemrograman bersama")
