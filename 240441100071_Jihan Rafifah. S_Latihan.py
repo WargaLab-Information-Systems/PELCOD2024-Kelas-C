bb775ddcd682da4e17b7d08cf139d29e511bd0b6
jihanrafifah78 <jihanrafifah630@gmail.com>
2024-09-07 11:51:06 +0700

    240441100071_Jihan Rafifah. S_ Latihan

---
+++ 240441100071_Jihan Rafifah. S_Latihan.py
@@ -0,0 +1,13 @@
+nama = "Jihan Rafifah Setiawan"
+umur = 19
+nilaiAlpro = 90,9
+
+hasil = f"Nama saya, {Jihan Rafifah Setiawan} Umur Saya {19} dan Nilai Alpro Saya {90,9}"
+print(hasil)
+print("Nama Saya", Jihan Rafifah Setiawan, "Umur", 19, "Nilai Alpro", 90,9)
+
+mandi = input ("Apakah kamu sudah mandi?")
+cuaca = input ("Cuaca sekarang seperti apa?")
+
+if mandi == "sudah" and cuaca == "cerah":
+    print ("Ayo berangkat ngampus")
