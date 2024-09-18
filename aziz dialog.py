7cc9fce6b9218a0fc4fedd879615082b5b29a8c9
Aziz101206 <abdaziz18526@gmail.com>
2024-09-19 02:11:34 +0700

    Add files via upload

---
+++ aziz dialog.py
@@ -0,0 +1,36 @@
+print("pada suatu hari yang cerah ada satu orang yang mengalami kesulitan dan ada orang menawarkan bantuan")
+nama1 = "aziz"
+nama2 = input("masukkan nama anda untuk berdialog dengan aziz")
+
+print("duarr terdengar suara ban motor pecah")
+print("aziz : waduh ban motorku pecah bagaimana ini") 
+print(nama2 , ": eh kamu bannya pecah ya")
+print("aziz : waduh iya nih gimana ya mana bengkelnya jauh lagi")
+input("klik enter untuk lanjut")
+print(nama2 , "apakah kamu butuh sedikit bantuan")
+print("aziz : hehehe iya nih butuh bantuan kamu tau bengkel di daerah sini?")
+input("klik enter untuk lanjut")
+print(nama2 , ":iya tau,dari sini kamu lurus aja apakah kamu mau di anter?")
+print("aziz :iya boleh kalo tidak keberatan dan merepotkan")
+print(nama2 , ":tentu saja tidak merepotkan")
+input("klik enter untuk lanjut")
+print("setibanya mereka di bengkel dan mereka berdua berkenalan")
+print("aziz : oo iya kita belum kenalan nama mu siapa?")
+print(nama2 , ":eh iyaa lo sampek lupa,namaku", nama2)
+
+
+print("beberapa saat kemudian bannya sudah di perbaiki")
+input("klik enter untuk lanjut")
+
+uang1 = 5000
+uang2 = 10000
+
+print(nama2 , ":eh sudah selesai uangmu cukup ngga?")
+print("aziz : waduh uangku cuma",uang1)
+input("klik enter untuk lanjut")
+print(nama2 , ":aman ini aku ada", uang2 , "pake aja dulu")
+print("aziz : waduh terimakasih banyak lo")
+input("klik enter untuk lanjut")
+print(nama2 , ":tidak masalah", nama1 , "aku pulang dulu ya kamu hati hati di jalan")
+print("aziz : iya terimakasih banyak", nama2 , "kamu hati hati di jalan sampai jumpa lagi")
+print(nama2 , ":sampai jumpa lagi",nama1)
