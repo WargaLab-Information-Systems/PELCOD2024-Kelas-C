46405abab508921deebea0b722e6819437c338cb
rizanurulmazidah <rzanrlmzda1096@gmail.com>
2024-09-19 15:01:30 +0700

    Add files via upload

---
+++ 240441100007_rizanurulmazidah.py
@@ -0,0 +1,21 @@
+print("selamat datang!")
+print("mohon masukka nama karakter")
+
+nama_pengguna1 = ("karakter1:")
+nama_pengguna2 = ("karakter2:")
+
+print(f"{nama_pengguna1}:hai{nama_pengguna2}selamat sore,dari mana?")
+makan = input(f"{nama_pengguna2}")
+print(f"{nama_pengguna1}:bisa anterin aku beli buah?")
+print(f"{nama_pengguna2}:tentu saja boleh")
+print(f"{nama_pengguna2}:depan lampu merah buahnya sangat murah!")
+print(f"{nama_pengguna1}:iyakah, ayo kita kesana!")
+print(f"{nama_pengguna1}:berapakah harga buah tadi?")
+
+#operasi aritmatika
+angka1 = int(input(f"{nama_pengguna2}:mangga seharga:"))
+angka2 = int(input(f"{nama_pengguna2}:manggis seharga"))
+jumlah = angka1 + angka2
+print(f"{nama_pengguna2}:jadi total harga{angka1} dan {angka2}adalah {jumlah}:")
+print(f"{nama_pengguna1}:baiklah terimakasih!")
+
