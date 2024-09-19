bab6c7be55340304c431044274751994926a1472
jihanrafifah78 <jihanrafifah630@gmail.com>
2024-09-19 14:43:36 +0700

    Add files via upload

---
+++ 240441100071_Jihan Rafifah. S_Tugas Pelcod.py
@@ -0,0 +1,24 @@
+print ("Di pagi hari yang cerah ini, Xeryn tiba tiba sakit karena dia sering begadang untuk mengerjakan tugas sekolah dari gurunya. Ia pun berkata kepada Ibunya bahwa ia sedang sakit demam. Lalu ibunya pun segera membelikannya obat di apotek.")
+
+print ("Tibalah Ibu Xeryn di apotek dekat rumahnya untuk membeli obat demam serta vitamin untuk memperkuat daya tahan tubuh sang anak.")
+
+nama_pengguna1 = ("Ibu")
+nama_pengguna2 = ("Apoteker")
+
+print(f"{nama_pengguna1}: Permisi mbak, ada obat untuk demam dan vitamin untuk menambah daya tahan tubuh ngga?")
+print(f"{nama_pengguna2}: {nama_pengguna1} mau obat merek apa?")
+print(f"{nama_pengguna1}: Untuk anak remaja, mbak. Tolong direkomendasikan saja, mbak")
+print(f"{nama_pengguna2}: Kalau rekomendasi dari saya itu, obat demam nya paracetamol sama vitamin nya itu imboost")
+print(f"{nama_pengguna1}: Iya mbak, saya beli 2 itu. Berapa total harganya, mbak?")
+
+#operasi aritmatika
+angka1 = int(input(f"{nama_pengguna2}:paracetamol seharga:"))
+angka2 = int(input(f"{nama_pengguna2}:imboost seharga:"))
+jumlah = angka1 + angka2
+print(f"{nama_pengguna2}: Jadi total harga {angka1} dan {angka2} adalah {jumlah}.")
+print(f"{nama_pengguna1}: Oke mbak, ini uangnya, terimakasih ya mbak.")
+print(f"{nama_pengguna2}: Sama-sama Ibu.")
+
+
+
+
